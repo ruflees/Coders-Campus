322ffd207830cd66d0f78950e0da8d8f08eb3852
ruflees <humphreymurbach@gmail.com>
2023-09-19 16:52:39 -0300

    first commit

---
+++ src/main/java/com/coderscampus/assignmentsubmission/AssignmentsubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.assignmentsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentsubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentsubmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/assignmentsubmission/AssignmentsubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.assignmentsubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentsubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
